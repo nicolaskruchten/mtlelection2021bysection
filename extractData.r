ebe069ceb0169bae5b7671dfe48cc150405aeefc
Nicolas Kruchten <nicolas@plot.ly>
2021-11-20 10:10:29 -0500

    full detailed map

---
+++ extractData.r
@@ -0,0 +1,14 @@
+library(reshape2)
+library(gtools)
+library(stringr)
+
+election2013 <- read.csv("raw_data.csv")
+election2013$early = ifelse(str_detect(election2013$Bureau, "[096]...?"), "early", "electionday")
+
+election2013$Bureau = gsub("a", "", election2013$Bureau)
+election2013$Bureau = gsub("b", "", election2013$Bureau)
+election2013$Bureau = factor(election2013$Bureau,mixedsort(levels(election2013$Bureau)))
+compacted = dcast(subset(election2013, early=="electionday"), 
+                  District+Poste+Candidat~Bureau, value.var="Votes", fun.aggregate=sum)
+compacted$District = as.numeric(lapply(strsplit(as.character(compacted$District), "-"), "[", 1))
+write.csv(compacted, file="data.csv", row.names=FALSE)
---
+++ makeTopo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+topojson --bbox --p id=NOM_SECTION --simplify-proportion=0.2 -o sections.topojson sectionelect.json
+cat data.csv | sed 's/,0,/,,/g' | sed 's/,0,/,,/g' | sed 's/,*$//g' | \
+sed 's/╘/È/g' | sed 's/Ω/Û/g' | sed 's/Γ/Ô/g' | sed 's/╖/À/g' | sed 's/╪/Ï/g'  > data2.csv
+mv data2.csv data.csv
